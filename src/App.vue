fbfe1138718bcb331814b76fe1fdd111ab31f61c
jaturongkp <jaturong254291@gmail.com>
2020-08-18 13:09:08 +0700

    game

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,59 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="card">
+      <div class="container">
+        <div id="app2">
+          <div class="card container">
+            <img alt="redTeam" src="./img/darth-vader-.png" width="200px" />
+            <HelloWorld msg="" />
+          </div>
+          <div class="card col-3">
+            <img alt="redTeam" src="./img/anakin-skywalker.png" width="200px" />
+            <HelloWorld msg="" />
+          </div>
+          <div class="card col-3">
+            <img alt="redTeam" src="./img/obi-wan.png" width="200px" />
+            <HelloWorld msg="" />
+          </div>
+        </div>
+      </div>
+
+      <div class="card col-3 container">
+        <img alt="redTeam" src="./img/iron_man.png" width="200px" />
+        <HelloWorld msg="" />
+      </div>
+      <div class="card col-3 container">
+        <img alt="redTeam" src="./img/captain-america.png" width="200px" />
+        <HelloWorld msg="" />
+      </div>
+      <div class="card col-3 container">
+        <img alt="redTeam" src="./img/Thor.png" width="200px" />
+        <HelloWorld msg="" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+//import fightButton from "./components/fightButton.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    //fightButton,
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  background-image: url(./img/chris-kesler.jpg);
+  background-size: cover;
+}
+#app2 {
   color: #2c3e50;
-  margin-top: 60px;
+  float: right;
 }
 </style>
